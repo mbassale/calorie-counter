8f25053abd8d5ed4b4f2ba1c44a7a0fff3335dc2
Marco Bassaletti <bassaletti@gmail.com>
2020-01-15 00:03:30 -0300

    Installed basic dependencies for Vue SPA: vue, vue-router and bootstrap-vue. Installed vuex for data management. Skeleton NavBar with router and content view.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+import App from './App.vue';
+import Dashboard from './views/dashboard/Dashboard.vue'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'dashboard',
+            component: Dashboard
+        }
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    render: (h) => h(App),
+    router,
+});
