269b54e361b0dc2d9a6f9e9b7e4c1ebdaa819cc9
Marco Bassaletti <bassaletti@gmail.com>
2020-01-25 23:38:12 -0300

    Calories per day must be greater or equal than 1.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- resources/js/views/meals/CaloriesPerDayForm.vue
+++ resources/js/views/meals/CaloriesPerDayForm.vue
@@ -3,7 +3,7 @@
         <b-form @submit="onSubmit" @reset="onReset">
             <b-form-group label="Calories Per Day" label-for="caloriesPerDay"
                           :state="$v.calories_per_day.$error ? false : null">
-                <b-form-input id="caloriesPerDay" type="number" min="0" step="1" placeholder="Calories Per Day"
+                <b-form-input id="caloriesPerDay" type="number" min="1" step="1" placeholder="Calories Per Day"
                               v-model="calories_per_day" :state="$v.calories_per_day.$error ? false : null"
                               :disabled="disabled"/>
             </b-form-group>
@@ -25,7 +25,7 @@
 
 <script>
     import _ from 'lodash';
-    import { required, numeric } from 'vuelidate/lib/validators';
+    import { required, numeric, minValue } from 'vuelidate/lib/validators';
     import { mapState } from 'vuex';
     import ToastMixin from '../../mixins/ToastMixin';
     import {GET_CURRENT_USER, UPDATE_USER} from '../../store/actions';
@@ -48,7 +48,8 @@
         validations: {
             calories_per_day: {
                 required,
-                numeric
+                numeric,
+                minValue: minValue(1)
             }
         },
         mounted() {
